3098b67efcd0d3e3b59144a6403584b71b610cd6
DONJUNIOR916 <junior.donfack@institutsaintjean.org>
2025-03-01 23:46:08 +0100

    Add files via upload

---
+++ btp/src/test/java/cm/sji/btp/BtpApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.sji.btp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
